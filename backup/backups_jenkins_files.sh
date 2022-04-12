0acf4d3e0aa8788fc9d4ee4c032fc4fd7f58fccb
Yazan Monshed <y.monshed@vardot.com>
2022-04-13 00:28:12 +0300

    update script backup

--- backup/backups_jenkins_files.sh
+++ backup/backups_jenkins_files.sh
@@ -2,15 +2,9 @@
 
 
 
-# backups jenkins files from container
+# Backup Jenkins Files From Container
 
 DATE=$(date "+%b-%d-%Y-%H-%M-%S") 
-
-
-
-
-echo "start jenkins files backups"
-
 
 docker cp jenkins:/var/jenkins_home/ /var/vardot/jenkins/jenkins_files_${DATE}
 
@@ -19,12 +13,12 @@ export AWS_SECRET_KEY=$2
 
 echo "upload jenkins files backups to S3..."
 
-aws s3 sync /var/vardot/jenkins/ s3://vardot-backups/ovh_backups/jenkins/backups_jenkins_conf/
+aws s3 sync /var/vardot/jenkins/ s3://<backet-name>/dir_backups/
 
 if [ $? -eq 0 ]; then
-	echo "upload to s3 is SESSFULLY!!"
+	echo "Upload to S3 is SUCCSSFULLY!"
 	exit 0;
 else
-	echo "upload to s3 is FAUILED!!!"
+	echo "Upload to S3 is FAILED!"
 fi
-echo "fhinshed."
+echo "compeleted."
